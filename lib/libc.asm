6bea18df5c6e9bd04c1b2c5e79a760e91e4bbab6
Sebastian Riedel <git@basxto.de>
2020-07-22 10:28:16 +0200

    Move trampoline to correct position

--- lib/libc.asm
+++ lib/libc.asm
@@ -1,8 +1,11 @@
 
 SECTION "Libc", ROM0
 
+MBC1_ROM_PAGE equ $2000
+
 _printf::
-	ret
+    ret
+
 
 banked_call::            ; Performs a long call.
     pop    hl        ; Get the return address
@@ -19,13 +22,26 @@ banked_call::            ; Performs a long call.
     ld    [$2000],a    ; Perform the switch
     ld    l,e
     ld    h,d
-    call .jump
+    call ___sdcc_call_hl
     pop    hl        ; Get the return address
     pop    af        ; Pop the old bank
     ld    [$2000],a ; rROMB0
     ldh    [__current_bank],a
-.jump
+___sdcc_call_hl::
     jp    hl
+
+___sdcc_bcall_ehl::
+    ldh a, [__current_bank]
+    push af
+    ld a, e
+    ldh [__current_bank], a
+    ld [MBC1_ROM_PAGE], a
+    call ___sdcc_call_hl
+___sdcc_breturn_ehl::
+    pop af
+    ld [MBC1_ROM_PAGE], a
+    ldh [__current_bank], a
+    ret
 
 SECTION "ROM bank", HRAM
 
